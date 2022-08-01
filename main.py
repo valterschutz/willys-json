6de760ee7940b88cec980215ee09169d1296e43f
Valter SchÃhÃhS chütz <valterschutz@protonmail.com>
2022-08-01 15:53:31 +0200

    Cleanup

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import sys
 import re
 import requests
 from time import strftime
+from os.path import exists
+
+OUTPUT_FILENAME = "data.json"
+
+# Data will only be generated for the following categories:
 URLS = [
     "https://www.willys.se/c/kott-chark-och-fagel?size=9999",
     "https://www.willys.se/c/mejeri-ost-och-agg?size=9999",
@@ -15,11 +20,13 @@ URLS = [
     "https://www.willys.se/c/dryck?size=9999",
     "https://www.willys.se/c/fardigmat?size=9999",
 ]
-# url = "https://www.willys.se/axfood/rest/p/101336170_ST"
 
+# A helper function
 def log(msg, indent=0):
     print(f"[{strftime('%X')}]: {'    ' * indent}{msg}")
 
+# This parses the json data for each product, only keeping values
+# of interest and doing type conversions
 def parse_product_json(json_data):
     d = {}
     try:
@@ -49,7 +56,7 @@ if __name__ == "__main__":
         for i, result in enumerate(category_products_json["results"]):
             log(f"Processing request #{i+1}/#{n_products}...", 1)
             code = result["code"]
-            url = "https://www.willys.se/axfood/rest/p/X".replace("X", code)
+            url = "https://www.willys.se/axfood/rest/p/" + str(code)
             request = requests.get(url)
             try:
                 json_data = request.json()
@@ -59,8 +66,6 @@ if __name__ == "__main__":
                 log(e)
 
     json_str = json.dumps(all_products)
-    f = open(sys.argv[1], "w")
+    f = open(OUTPUT_FILENAME, "w")
     f.write(json_str)
     f.close()
-# print(products)
-
